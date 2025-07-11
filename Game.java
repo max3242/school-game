30f6d615a8277bf1957877e58d5bf192032b020b
max3242 <champinemd@gmail.com>
2025-07-11 05:52:45 +0000

    a

--- Game.java
+++ Game.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
+//import java.util.Arrays;
 public class Game{
    int dscor=0;
    int multi=1;
-   int req=1;
+   int req=10;
    int scor=0;
    ArrayList<Card> d=new ArrayList<Card>();
    ArrayList<Card> d1=new ArrayList<Card>();
@@ -27,8 +27,9 @@ public class Game{
            d.add(new Card("d",i));
            d1.add(new Card("d",i));
        }
-       while(scor>=req||(req==1&&scor==0)){
-           round();
+       while(scor>=req||(req==10&&scor==0)){
+            req+=1;
+            round();
        }
    }
    public void round(){
@@ -36,6 +37,7 @@ public class Game{
        hand.clear();
        p.clear();
        scor=0;
+       multi=1;
        for(Card c:d1){
            d.add(c);
        }
@@ -54,22 +56,53 @@ public class Game{
        System.out.println("");
        Scanner s1=new Scanner(System.in);
        while (!in.equals("done")) {
-           System.out.println("(Type 'close' to finish the hand, Type 'done' to go to scoring) Enter the index of the card you want to play:");
+           System.out.println("(Type 'close' to finish the pool, Type 'done' to go to scoring) Enter the index of the card you want to play:");
            in = s1.nextLine();
              if (in.equals("done")) {
-               System.out.println("Finished with the hand.");
+               for(Card c:p.get(pol).getPool()){
+                scor+=(int)(c.v());
+               }
+               if(p.get(pol).getPool().size()==1){
+                if(p.get(pol).getPool().get(0).v()==8){
+                    while(hand.size()>0){
+                       d.add(hand.remove(0));
+                    }
+                    for(int i=0;i<7;i++){
+                        x=(int)(Math.random()*(d.size()));
+                        hand.add(d.remove(x));
+                    }
+                }else if(p.get(pol).getPool().get(0).v()==7&&p.get(pol).getPool().get(0).c().equals("r")){
+                    multi*=7;
+                }
+               }
+               System.out.println("Finished with hand.");
+               scor*=multi;
+               System.out.println("Req: "+req+" Score: "+scor);
                break;
             }
            if (in.equals("close")) {
                System.out.println("Pile finished. Moving to next pile...");
+               for(Card c:p.get(pol).getPool()){
+                scor+=(int)(c.v());
+               }
+               if(p.get(pol).getPool().get(0).v()==8&&p.get(pol).getPool().size()==1){
+                    while(hand.size()>0){
+                       d.add(hand.remove(0));
+                    }
+                    for(int i=0;i<7;i++){
+                        x=(int)(Math.random()*(d.size()));
+                        hand.add(d.remove(x));
+                    }
+               }else if(p.get(pol).getPool().get(0).v()==7&&p.get(pol).getPool().size()==1&&p.get(pol).getPool().get(0).c().equals("r")){
+                    multi*=7;
+               }
                pol+=1;
                p.add(new Pool());
-               
            }else if (!in.equals("close")&&!in.equals("done")) {
                try {
                int index = Integer.parseInt(in);
-                   if (index >= 0 && index < hand.size()) {
-                       p.get(pol).ad(hand.remove(index));
+                   if (index > 0 && index < hand.size()+1) {
+                       p.get(pol).ad(hand.remove(index-1));
                    } else {
                        System.out.println("Invalid index. Please choose a valid card index.");
                    }
@@ -88,7 +121,6 @@ public class Game{
            }
            System.out.println("");
        }
-       req+=1;
        
 
 
